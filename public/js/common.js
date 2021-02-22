0d4d04c6ca68f24d22c46cf473364a1c43fa1ce4
wjsdhtkd <77598150+wjsdhtkd@users.noreply.github.com>
2021-02-22 17:10:52 +0900

    js수정
    수정

--- public/js/common.js
+++ public/js/common.js
@@ -15,4 +15,18 @@ $(function() {
         }
         //console.log(st);
     })
+    $("html, body").on('touchmove', function(e){
+        var wheel = e.originalEvent.wheelDelta;
+        var He  = $('header').height();
+        var st = $(window).scrollTop();
+        // e.preventDefault();
+        if(st > 0) {
+            $(".main-header").css("background-color", "#0c2f48");
+        }
+        if (st <= 35) {
+            $(".main-header").css("background", "rgba(0,0,0,0)");
+        }
+        //console.log(st);
+    })
+
 })
