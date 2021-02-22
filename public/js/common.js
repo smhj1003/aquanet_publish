5ab911fdfbbf2d56ba637fb0db4af7c8cc67a02c
wjsdhtkd <77598150+wjsdhtkd@users.noreply.github.com>
2021-02-22 15:35:35 +0900

    02/22 수정
    수정

---
+++ public/js/common.js
@@ -0,0 +1,18 @@
+$(function() {
+    var lastScrollTop = 0;
+    var delta = 1;
+    var navbarHeight = $('header').outerHeight();
+    $("html, body").on('mousewheel', function(e){
+        var wheel = e.originalEvent.wheelDelta;
+        var He  = $('header').height();
+        var st = $(window).scrollTop();
+        // e.preventDefault();
+        if(st > 0) {
+            $(".main-header").css("background-color", "#0c2f48");
+        }
+        if (st <= 35) {
+            $(".main-header").css("background", "rgba(0,0,0,0)");
+        }
+        //console.log(st);
+    })
+})
