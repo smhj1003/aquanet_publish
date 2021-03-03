6693a3ab485127e19dddd9162e9f3c1e33c76625
wjsdhtkd <77598150+wjsdhtkd@users.noreply.github.com>
2021-03-03 11:06:07 +0900

    Update common.js

--- public/js/common.js
+++ public/js/common.js
@@ -17,29 +17,57 @@ $(function() {
         }
         //console.log(st);
     })
-    $("html, body").on('touchmove', function(e){
-        var wheel = e.originalEvent.wheelDelta;
-        var He  = $('.main-header').scrollTop();
-        var st = $(window).scrollTop();
-        var scrollValue = $(document).scrollTop();
-        // e.preventDefault();
-        if(scrollValue > 25) {
-            $(".main-header").css("background-color", "#0c2f48");
-        }
-        if (scrollValue < 20) {
-            $(".main-header").css("background", "rgba(0,0,0,0)");
-        }
-        if (scrollValue = 0){
-            setInterval(function() {
-                $(".main-header").css("background", "rgba(0,0,0,0)");
-            },1);
+    // $("html, body").on('touchmove', function(e){
+    //     var wheel = e.originalEvent.wheelDelta;
+    //     var He  = $('.main-header').scrollTop();
+    //     var st = $(window).scrollTop();
+    //     var scrollValue = $(document).scrollTop();
+    //     // e.preventDefault();
+    //     if(scrollValue > 25) {
+    //         $(".main-header").css("background-color", "#0c2f48");
+    //     }
+    //     if (scrollValue < 20) {
+    //         $(".main-header").css("background", "rgba(0,0,0,0)");
+    //     }
+    //     if (scrollValue = 0){
+    //         setInterval(function() {
+    //             $(".main-header").css("background", "rgba(0,0,0,0)");
+    //         },1);
 
-        }
-    })
-    $("html, body").on('touchend-', function(e){
-        var scrollValue = $(document).scrollTop();
-        if(scrollValue <= 10 ){
-            $(".main-header").css("background", "rgba(0,0,0,0)");
-        }
+    //     }
+    // })
+    // $("html, body").on('touchend-', function(e){
+    //     var scrollValue = $(document).scrollTop();
+    //     if(scrollValue <= 10 ){
+    //         $(".main-header").css("background", "rgba(0,0,0,0)");
+    //     }
+    // });
+    // $(function(){
+    //     var lastScrollTop = 0;
+    //         delta =15;
+    //         $(window).touchmove(function(e){
+    //             var st = $(this).scrollTop();
+    //             if(Math.abs(lastScrollTop -st) <= delta)return;
+    //             if((st>lastScrollTop)&&(lastScrollTop>0)) {
+    //                 $(".main-header").css("background-color", "#0c2f48");
+    //             } else {
+    //                 $(".main-header").css("background", "rgba(0,0,0,0)");
+    //             }
+    //             lastScrollTop=st;
+    //         });
+    // });
+    $(function(){
+        var lastScrollTop = 0;
+            delta =15;
+            $(window).on("touchmove", function(e){
+                var st = $(this).scrollTop();
+                if(Math.abs(lastScrollTop -st) <= delta)return;
+                if((st>lastScrollTop)&&(lastScrollTop>0)) {
+                    $(".main-header").css("background-color", "#0c2f48");
+                } else if($(window).scrollTop() <= 10) {
+                    $(".main-header").css("background", "rgba(0,0,0,0)");
+                }
+                lastScrollTop=st;
+            });
     });
 })
